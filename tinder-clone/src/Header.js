281c4eb7019781e2c22369909222aeb67ddc3f2d
yasin-herken <yasin.herken123@gmail.com>
2022-06-21 12:39:26 +0300

    Tinder added as a repositery

---
+++ tinder-clone/src/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Header.css";
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
+import logo from "./logo.png";
+function Header() {
+  return (
+    <div className='header'>
+         <IconButton>
+            <PersonIcon fontSize="large" className="header__icon"/>
+         </IconButton>
+         <img 
+         className="header__logo"
+         src={logo}   
+         alt="Logo does not exists"     
+         />
+         <IconButton>
+            <ForumIcon fontSize="large" className="header__icon"/>
+         </IconButton>
+        
+         
+    </div>
+  )
+}
+
+export default Header
